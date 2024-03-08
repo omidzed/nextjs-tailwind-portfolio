12f85f18a6512ba14ddd597a6bee180c3fd8e056
Omid Asadi <omiddzedd@gmail.com>
2024-03-07 21:04:21 -0800

    removed template github stats

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,5 +1,4 @@
 import { motion } from 'framer-motion';
-import AboutCounter from '../components/about/AboutCounter';
 import AboutMeBio from '../components/about/AboutMeBio';
 import PagesMetaHead from '../components/PagesMetaHead';
 
@@ -23,7 +22,6 @@ function about() {
 				animate={{ opacity: 1, delay: 1 }}
 				exit={{ opacity: 0 }}
 			>
-				<AboutCounter />
 			</motion.div>
 
 			<motion.div
